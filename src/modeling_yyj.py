10b4b4fa4d828d8973e77483b145ed9853aea2e5
yeongjae-yu <yyj3053@gmail.com>
2021-01-15 00:59:27 +0900

    20210115 study self attention

--- src/modeling_yyj.py
+++ src/modeling_yyj.py
@@ -3,7 +3,7 @@ import logging
 import math
 
 import torch
-from torch import nn
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss
 
@@ -40,20 +40,26 @@ class LayerNorm(nn.Module):
 class Embedding(nn.Module):
     def __init__(self, config):
         super(Embedding, self).__init__()
+        # [vocab_size, hidden_size]
         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=0)
+        # [max_position_embeddings, hidden_size]
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        # [type_vocab_size, hidden_size]
         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
 
         self.LayerNorm = LayerNorm(config.hidden_size, config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, input_ids, token_type_ids=None):
+        # input_ids shape is [batch_size, seq_length]
         seq_length = input_ids.size(1)
+        # position_ids shape becomes [batch_size, seq_length]
         position_ids = torch.arange(seq_length, dtype=torch.long, device=input_ids.device)
         position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
+        # token_type_ids shape becomes [batch_size, seq_length]
         if token_type_ids is None:
             token_type_ids = torch.zeros_like(input_ids)
-
+        # [batch_size, seq_length, hidden_size]
         word_embeddings = self.word_embeddings(input_ids)
         position_embeddings = self.position_embeddings(position_ids)
         token_type_embeddings = self.token_type_embeddings(token_type_ids)
@@ -61,7 +67,41 @@ class Embedding(nn.Module):
         embeddings = word_embeddings + position_embeddings + token_type_embeddings
         embeddings = self.LayerNorm(embeddings)
         embeddings = self.dropout(embeddings)
+        # embeddings shape is [batch_size, seq_length, hidden_size]
         return embeddings
 
+
+class SelfAttention(nn.Module):
+    def __init__(self, config):
+        super(SelfAttention, self).__init__()
+        if config.hidden_size % config.num_attention_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (config.hidden_size, config.num_attention_heads))
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        # [hidden_size, hidden_size]
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states, attention_mask):
+        # [hidden_size, hidden_size]
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
 
 
