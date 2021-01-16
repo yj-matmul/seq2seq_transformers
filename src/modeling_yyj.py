496d9b8d81b60e7e961825800c5b0661d65ca2ed
yeongjae-yu <yyj3053@gmail.com>
2021-01-16 23:50:39 +0900

    [20210116] write encoder of transformers

--- src/modeling_yyj.py
+++ src/modeling_yyj.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import math
+import copy
 
 import torch
 import torch.nn as nn
@@ -95,13 +96,103 @@ class SelfAttention(nn.Module):
         return x.permute(0, 2, 1, 3)
 
     def forward(self, hidden_states, attention_mask):
-        # [hidden_size, hidden_size]
+        # [batch_size, seq_length, hidden_size]
         mixed_query_layer = self.query(hidden_states)
         mixed_key_layer = self.key(hidden_states)
         mixed_value_layer = self.value(hidden_states)
 
+        # [batch_size, num_attention_heads, seq_length, attention_head_size]
         query_layer = self.transpose_for_scores(mixed_query_layer)
         key_layer = self.transpose_for_scores(mixed_key_layer)
         value_layer = self.transpose_for_scores(mixed_value_layer)
 
+        # [batch_size, num_attention_heads, seq_length, seq_length]
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_scores += attention_mask
+
+        # [batch_size, num_attention_heads, seq_length, seq_length]
+        attention_probs = nn.Softmax(dim=1)(attention_scores)
+
+        # [batch_size, num_attention_heads, seq_length, attention_head_size]
+        context_layer = torch.matmul(attention_probs, value_layer)
+        # [batch_size, seq_length, num_attention_heads, attention_head_size]
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        # [batch_size, seq_length, hidden_size]
+        context_layer = context_layer.view(*new_context_layer_shape)
+        return context_layer
+
+
+class SelfOutput(nn.Module):
+    def __init__(self, config):
+        super(SelfOutput, self).__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.LayerNorm = LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, hidden_states, input_tensor):
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+        return hidden_states
+
+
+class EncoderAttention(nn.Module):
+    def __init__(self, config):
+        super(EncoderAttention, self).__init__()
+        self.self_attention = SelfAttention(config)
+        self.self_output = SelfOutput(config)
+
+    def forward(self, input_tensor, attention_mask):
+        self_attention = self.self_attention(input_tensor, attention_mask)
+        attention_output = self.self_output(self_attention, input_tensor)
+        return attention_output
+
+
+class FeedForward(nn.Module):
+    def __init__(self, config):
+        super(FeedForward, self).__init__()
+        self.dense1 = nn.Linear(config.hidden_size, config.feedforward_size)
+        self.feedforward_act = config.hidden_act
+        self.dense2 = nn.Linear(config.feedforward_size, config.hidden_size)
+        self.LayerNorm = LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, hidden_states, input_tensor):
+        hidden_states = self.dense1(hidden_states)
+        hidden_states = self.feedforward_act(hidden_states)
+        hidden_states = self.dense2(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+        return hidden_states
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, config):
+        super(EncoderLayer, self).__init__()
+        self.encoder_attention = EncoderAttention(config)
+        self.feedforward = FeedForward(config)
+
+    def forward(self, hidden_states, attention_mask):
+        attention_output = self.encoder_attention(hidden_states, attention_mask)
+        layer_output = self.feedforward(attention_output, attention_output)
+        return layer_output
+
+
+class AllEncoders(nn.Module):
+    def __init__(self, config):
+        super(AllEncoders, self).__init__()
+        layer = EncoderLayer(config)
+        self.layer = nn.ModuleList([copy.deepcopy(layer)] for _ in range(config.num_hidden_layers))
+
+    def forward(self, hidden_states, attention_mask, output_all_encoded_layers=True):
+        all_encoder_layers = []
+        for layer_module in self.layer:
+            hidden_states = layer_module(hidden_states, attention_mask)
+            if output_all_encoded_layers:
+                all_encoder_layers.append(hidden_states)
+        if not output_all_encoded_layers:
+            all_encoder_layers.append(hidden_states)
+        return all_encoder_layers
 
