85c5ac2c91113740677be2495ce789405447a17a
yeongjae-yu <yyj3053@gmail.com>
2021-01-18 02:17:53 +0900

    [20210118] add config and reference other's transformer code

--- src/modelTransformer.py
+++ src/modelTransformer.py
@@ -19,4 +19,42 @@ def get_sinusoid_encoding_table(seq_length, hidden_size):
     sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])
     sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])
 
-    return sinusoid_table
+    return torch.FloatTensor(sinusoid_table)
+
+
+class TransformerConfig():
+    def __init__(self,
+                 encoder_vocab_size,
+                 decoder_vocab_size,
+                 hidden_size=512,
+                 num_hidden_layers=6,
+                 num_attention_heads=8,
+                 hidden_act='relu',
+                 feed_forward_size=2048,
+                 hidden_dropout_prob=0.1,
+                 attention_dropout_prob=0.1,
+                 max_seq_length=512,
+                 initializer_range=0.02,
+                 layer_norm_eps=1e-12):
+        self.encoder_vocab_size = encoder_vocab_size
+        self.decoder_vocab_size = decoder_vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.hidden_act = hidden_act
+        self.feed_forward_size = feed_forward_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_dropout_prob
+        self.max_position_embeddings = max_seq_length
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
+
+if __name__ == '__main__':
+    config = TransformerConfig(100,
+                               100)
+    inputs = torch.tensor([[3091, 3604,  206, 3958, 3760, 3590,    0,    0],
+                           [ 212, 3605,   53, 3832, 3596, 3682, 3760, 3590]])
+
+    print(inputs.eq(0).unsqueeze(1).size())
+    print(inputs.eq(0).unsqueeze(1))
+    inputs.masked_fill_
