0ccf4863110893e43059cf1beb408d73e5e9c5e6
yeongjae-yu <yyj3053@gmail.com>
2021-01-26 23:21:34 +0900

    [20210126] refactoring parameter names and test various parameter values

--- src/run_transformer_yyj.py
+++ src/run_transformer_yyj.py
@@ -4,7 +4,7 @@ from transformer_yyj import Transformer, TransformerConfig
 from torch import optim
 
 if __name__ == '__main__':
-    vocab_size = 100
+    vocab_size = 1000
     config = TransformerConfig(src_vocab_size=vocab_size,
                                trg_vocab_size=vocab_size,
                                device='cuda')
@@ -22,8 +22,10 @@ if __name__ == '__main__':
     for epoch in range(total_epoch):
         optimizer.zero_grad()
         outputs, _ = model(inputs, labels)
-        outputs = outputs.contiguous().view(-1, vocab_size)
-        target = labels.contiguous().view(-1).long()
+        # outputs = outputs.contiguous().view(-1, vocab_size)
+        # target = labels.contiguous().view(-1).long()
+        outputs = outputs.view(-1, vocab_size)
+        target = labels.view(-1).long()
         loss = criterion(outputs, target)
         if epoch % 10 == 9:
             print('Epoch: %3d ' % (epoch + 1), '\tCost: {:.5f}'.format(loss))
@@ -36,4 +38,4 @@ if __name__ == '__main__':
     print(predict.size())
     print(labels.size())
     print('predict\n', predict[0])
-    print('labes\n', labels[0])
+    print('labels\n', labels[0])
